764bb0a7ebe4c25a43830bcd44aabe3d0173b6a2
Vlad Zhalin <vlad.zhalin97@gmail.com>
2022-09-04 16:59:49 +0200

    Id field, specific product api, store stores unique items with different quantities

--- backend/data.js
+++ backend/data.js
@@ -1,7 +1,8 @@
 const data = {
   products: [
     {
-      name: 'Nike Slim Shirt',
+      _id: '1',
+      name: 'Nikes Slim Shirt',
       slug: 'nike-slim-shirt',
       category: 'Shirts',
       image: '/images/p1.jpg',
@@ -13,6 +14,7 @@ const data = {
       description: 'high quality shirt',
     },
     {
+      _id: '2',
       name: 'Adidas Fit Shirt',
       slug: 'adidas-fit-shirt',
       category: 'Shirts',
@@ -25,6 +27,7 @@ const data = {
       description: 'high quality product',
     },
     {
+      _id: '3',
       name: 'Nike Slim Pant',
       slug: 'nike-slim-pant',
       category: 'Pants',
@@ -37,6 +40,7 @@ const data = {
       description: 'high quality product',
     },
     {
+      _id: '4',
       name: 'Adidas Fit Pant',
       slug: 'adidas-fit-Pant',
       category: 'Pants',
